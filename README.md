6da5ea6fedd9cb43e3579ffccc37f1eb1f88914f
Jerson Pool Miranda Robles <jmirandr@LIM-TXHY5HL62Y.local>
2023-03-24 11:54:11 -0500

    feat: api spam available

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # spam-dectector
+
+Start app with next commands:
 ```bash
 npm install
 node index.js
+```
+Call api ```http://127.0.0.1:3000/predict``` as POST method with body (example):
+
+```json
+{
+    "message": "mano a que hora vienes por el pan" 
+}
 ```
--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 // import * as tf from '@tensorflow/tfjs';
+const express = require('express');
 const tf = require('@tensorflow/tfjs-node');
 const tokenize = require('./tokenize');
 
+let model;
+const app = express();
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.set('port', 3000);
+
+app.post('/predict', async (req, res) => {    
+    const message = req.body.message;
+    const result = await loadModel(message);
+    res.json(result).end();
+})
+
 const loadModel = async (message) => {
-    const model = await tf.loadLayersModel('https://assetshub.blob.core.windows.net/models/model.json');
     const lowercaseSentenceArray = message.toLowerCase().replace(/[^\w\s]/g, ' ').split(' ');
     const inputTensor = tf.tensor(tokenize(lowercaseSentenceArray));
-    // console.log(inputTensor);
     const prediction = model.predict(inputTensor);
-    // console.log(prediction);
-    // console.log(prediction.dataSync());
-    const sentiment = prediction.dataSync()[1] > 0.5 ? 'SPAM' : 'NO SPAM';
-    console.log(`${message}: ${sentiment}`);
+    const result = prediction.dataSync()[1] > 0.5 ? 'SPAM' : 'NO SPAM';
+    console.log(`${message}: ${result}`);
+    return { result };
 }
 
-loadModel("mano a que hora vienes por el pan");
-loadModel("BCP te informa que tienes una deuda pendiente, ingresa a este enlace http");
-loadModel("Tu tarjeta ha sido bloqueada, por favor enviame tu toquen");
-loadModel("tu tarjeta ha sido bloqueada, por favor enviame tu token o comunicate al 999");
-loadModel("llamaste a pablo? avisame porfa");
+// loadModel("mano a que hora vienes por el pan");
+// loadModel("BCP te informa que tienes una deuda pendiente, ingresa a este enlace http");
+// loadModel("Tu tarjeta ha sido bloqueada, por favor enviame tu toquen");
+// loadModel("tu tarjeta ha sido bloqueada, por favor enviame tu token o comunicate al 999");
+// loadModel("llamaste a pablo? avisame porfa");
+
+app.listen(app.get('port'), async ()=> {
+    console.log(`Server listening on port ${app.get('port')}`);
+    model = await tf.loadLayersModel('https://assetshub.blob.core.windows.net/models/model.json');
+});
